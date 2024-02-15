557f59a63619cf4378153b6b4c6131839791eacc
RichardKaranuMbuti <Officialforrichardk@gmail.com>
2024-02-15 12:56:56 +0300

    Variables and Identifiers

---
+++ intro/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main (String[] args){
+        System.out.println("This is java");
+        int myNum = 22 ;
+        final int myNum2 = 56 ;
+        char myChar = 'A' ;
+        String myName = "Richie" ;
+        float myFloat = 31.678f ;
+        System.out.println("My Number:" + myName);
+    }
+}
